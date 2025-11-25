27b10afa1907c9bfde17a50f3fd01aab835159c9
dongyuanxin <yuanxin.me@gmail.com>
2025-11-25 13:33:56 +0800

    新增try with resources

---
+++ src/main/java/oop/demo/trywithresources/MyResourceDemo1.java
@@ -0,0 +1,32 @@
+package oop.demo.trywithresources;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+// try-with-resources: 都必须实现 Closeable 接口，重写 close() 方法
+
+public class MyResourceDemo1 {
+    public static void main(String[] args) {
+        try (RealResource r = new RealResource()) {
+            r.doSomething();
+        } catch (IOException e) {
+            System.out.println("error occur: " + e);
+        }
+
+    }
+}
+
+class RealResource implements Closeable {
+    public RealResource() {
+        System.out.println("create");
+    }
+
+    public void doSomething() {
+        System.out.println("doSomething");
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("close");
+    }
+}
